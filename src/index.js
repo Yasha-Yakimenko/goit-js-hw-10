bbd3bd3955074f69b665c0ef8036cbe97861ac25
Yasha-Yakimenko <yasha.yakimenko77@gmail.com>
2022-06-04 00:24:06 +0300

    .
    .

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
-
+import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    countryInput: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+  };
+
+  refs.countryInput.addEventListener('input', debounce(onCountryInput, DEBOUNCE_DELAY));
+
+  
